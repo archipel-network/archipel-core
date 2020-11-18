c96f66e4817d18c781dfdbfbfd452d5ee9c761d5
Felix Walter <felix.walter@d3tn.com>
2020-11-18 11:52:53 +0100

    cla_tcp_common: Always close() TCP sockets on disconnect
    In the case of a graceful disconnect of the other peer or if an error
    was encountered during recv() or send(), cla_disconnect_handler() is
    called on the cla_link. However, the existing handler did not close()
    the TCP socket. This led to an exhaustion of file descriptors after many
    connections.
    
    This commit adds a TCP-specific disconnect handler which calls close()
    on the socket to release the associated resources.
    
    Signed-off-by: Felix Walter <felix.walter@d3tn.com>

--- components/cla/posix/cla_tcpclv3.c
+++ components/cla/posix/cla_tcpclv3.c
@@ -703,7 +703,7 @@ const struct cla_vtable tcpclv3_vtable = {
 
 	.cla_read = cla_tcp_read,
 
-	.cla_disconnect_handler = cla_generic_disconnect_handler,
+	.cla_disconnect_handler = cla_tcp_disconnect_handler,
 };
 
 static enum ud3tn_result tcpclv3_init(
