92a5e7f680b624075c5b078a19062f35c7c525f6
Felix Walter <felix.walter@d3tn.com>
2021-07-22 16:40:14 +0200

    stylecheck: Update line length and ignored checks
    This...
    - sets the line length limit to 100 chars in accordance with the new
      Linux kernel line length limit
    - ignores things we have no control or which do not apply to us: camel
      case (used via TinyCBOR, simplequeue, and FreeRTOS), and macros and types
      we have not implemented (things only part of the Linux kernel)
    
    Signed-off-by: Felix Walter <felix.walter@d3tn.com>

--- tools/analysis/stylecheck.sh
+++ tools/analysis/stylecheck.sh
@@ -3,8 +3,10 @@
 
 CHECKPATCH_FLAGS="--no-tree --terse --show-types --ignore AVOID_EXTERNS \
 --ignore COMPLEX_MACRO --ignore NEW_TYPEDEFS --ignore FUNCTION_ARGUMENTS \
---ignore SPDX_LICENSE_TAG --ignore LONG_LINE_STRING --ignore REPEATED_WORD \
---typedefsfile ./tools/analysis/stylecheck_typedefs.txt --emacs"
+--ignore SPDX_LICENSE_TAG --ignore PREFER_DEFINED_ATTRIBUTE_MACRO --ignore REPEATED_WORD \
+--ignore PREFER_KERNEL_TYPES --ignore LONG_LINE_STRING --ignore CAMELCASE \
+--typedefsfile ./tools/analysis/stylecheck_typedefs.txt --emacs --file \
+--max-line-length=100"
 
 S=0
 
@@ -19,14 +21,14 @@ for check_dir in "${check_dirs[@]}"; do
 		if [ $cnt != 0 ]
 		then
 			echo "Checking $dir/*.c..."
-			$CHECKPATCH $CHECKPATCH_FLAGS --file $dir/*.c
+			$CHECKPATCH $CHECKPATCH_FLAGS $dir/*.c
 			S=$(($S + $?))
 		fi
 		cnt=`ls -1 $dir/*.h 2>/dev/null | wc -l`
 		if [ $cnt != 0 ]
 		then
 			echo "Checking $dir/*.h..."
-			$CHECKPATCH $CHECKPATCH_FLAGS --file $dir/*.h
+			$CHECKPATCH $CHECKPATCH_FLAGS $dir/*.h
 			S=$(($S + $?))
 		fi
 	done
