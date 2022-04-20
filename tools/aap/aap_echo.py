0de5650206210f9014f8a53c0a3677c229711c34
Oleksandr Nazymko <oleksandr.nazymko@d3tn.com>
2022-04-20 14:54:18 +0200

    Implemented aap_echo function
    It implements a simple AAP application that connects to a DTN-node and sends
    an "echo" of the received bundles back to the sender. This tool works well with
    a companion aap_ping function "ping" receiving "ping bundles" and sending
    them back(like in the ICMP echos).
    
    Resolves: #80
    See also: #79
    
    Signed-off-by: Oleksandr Nazymko <oleksandr.nazymko@d3tn.com>

---
+++ tools/aap/aap_echo.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: BSD-3-Clause OR Apache-2.0
+# encoding: utf-8
+
+"""
+A tool that sends received bundles back to the sender.
+"""
+
+import sys
+import argparse
+import logging
+
+
+from ud3tn_utils.aap import AAPUnixClient, AAPTCPClient, AAPMessage
+from helpers import add_common_parser_arguments, logging_level
+from ud3tn_utils.aap.aap_message import AAPMessageType
+
+
+def run_echo(aap_client):
+
+    print(
+        f"Registred EID {aap_client.eid}"
+        f"\nWaiting for bundles..."
+    )
+
+    while True:
+
+        try:
+
+            msg = aap_client.receive()
+        except KeyboardInterrupt:
+
+            print("Quiting")
+            return
+
+        if not msg:
+
+            print("Lost connection, quitting")
+            sys.exit(1)
+
+        if msg.msg_type == AAPMessageType.RECVBUNDLE:
+
+            print(
+                f"-> Received bundle of length {len(msg.payload)} byte"
+                f" from {msg.eid} "
+            )
+
+            try:
+                aap_client.socket.send(
+                    AAPMessage(
+                        AAPMessageType.SENDBUNDLE,
+                        msg.eid,
+                        msg.payload
+                    ).serialize()
+                )
+                print(
+                    f"-> Have sent a bundle back to {msg.eid}"
+                )
+            except InterruptedError:
+                print("An interruption error occured!")
+                return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="sends received bundles back to the sender",
+    )
+
+    add_common_parser_arguments(parser)
+
+    args = parser.parse_args()
+
+    if args.verbosity:
+        logging.basicConfig(level=logging_level(args.verbosity))
+
+    if args.tcp:
+        addr = (args.tcp[0], int(args.tcp[1]))
+        with AAPTCPClient(address=addr) as aap_client:
+            aap_client.register(args.agentid)
+            run_echo(aap_client)
+    else:
+        with AAPUnixClient(address=args.socket) as aap_client:
+            aap_client.register(args.agentid)
+            run_echo(aap_client)
