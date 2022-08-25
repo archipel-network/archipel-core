0d04a6ff2f1bcd3de2b514e9bd5c6265639bfc8e
Felix Walter <felix.walter@d3tn.com>
2022-08-25 09:49:37 +0200

    agents: Move bundle creation functions to utility module
    This moves the functions to create bundles from within agents from the
    application agent to a dedicated utility module, ready to be re-used by
    other agents.
    
    Signed-off-by: Felix Walter <felix.walter@d3tn.com>

---
+++ components/ud3tn/agent_util.c
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: BSD-3-Clause OR Apache-2.0
+#include "bundle6/create.h"
+#include "bundle7/create.h"
+
+#include "ud3tn/agent_util.h"
+#include "ud3tn/common.h"
+#include "ud3tn/bundle.h"
+#include "ud3tn/bundle_agent_interface.h"
+#include "ud3tn/bundle_processor.h"
+#include "ud3tn/bundle_storage_manager.h"
+#include "ud3tn/eid.h"
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct bundle *agent_create_bundle(const uint8_t bp_version,
+	const char *local_eid, char *sink_id, char *destination,
+	const uint64_t creation_timestamp_s, const uint64_t sequence_number,
+	const uint64_t lifetime, void *payload, size_t payload_length,
+	enum bundle_proc_flags flags)
+{
+	const size_t local_eid_length = strlen(local_eid);
+	const size_t sink_length = strlen(sink_id);
+	char *source_eid = malloc(local_eid_length + sink_length + 2);
+
+	if (source_eid == NULL) {
+		free(payload);
+		return NULL;
+	}
+
+	memcpy(source_eid, local_eid, local_eid_length);
+	if (get_eid_scheme(source_eid) == EID_SCHEME_IPN) {
+		char *const dot = strchr(source_eid, '.');
+
+		ASSERT(dot != NULL);
+		memcpy(&dot[1], sink_id, sink_length + 1);
+	} else {
+		memcpy(&source_eid[local_eid_length],
+		       sink_id, sink_length + 1);
+	}
+
+	struct bundle *result;
+
+	if (bp_version == 6)
+		result = bundle6_create_local(
+			payload, payload_length, source_eid, destination,
+			creation_timestamp_s, sequence_number,
+			lifetime, flags);
+	else
+		result = bundle7_create_local(
+			payload, payload_length, source_eid, destination,
+			creation_timestamp_s, sequence_number,
+			lifetime, flags);
+
+	free(source_eid);
+
+	return result;
+}
+
+bundleid_t agent_create_forward_bundle(
+	const struct bundle_agent_interface *bundle_agent_interface,
+	const uint8_t bp_version, char *sink_id, char *destination,
+	const uint64_t creation_timestamp_s, const uint64_t sequence_number,
+	const uint64_t lifetime, void *payload, size_t payload_length,
+	enum bundle_proc_flags flags)
+{
+	struct bundle *bundle = agent_create_bundle(
+		bp_version,
+		bundle_agent_interface->local_eid,
+		sink_id,
+		destination,
+		creation_timestamp_s,
+		sequence_number,
+		lifetime,
+		payload,
+		payload_length,
+		flags
+	);
+
+	if (bundle == NULL)
+		return BUNDLE_INVALID_ID;
+
+	bundleid_t bundle_id = bundle_storage_add(bundle);
+
+	if (bundle_id != BUNDLE_INVALID_ID)
+		bundle_processor_inform(
+			bundle_agent_interface->bundle_signaling_queue,
+			bundle_id,
+			BP_SIGNAL_BUNDLE_LOCAL_DISPATCH,
+			BUNDLE_SR_REASON_NO_INFO
+		);
+	else
+		bundle_free(bundle);
+
+	return bundle_id;
+}
