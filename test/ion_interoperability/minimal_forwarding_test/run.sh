4c33209557e2aae0a954279ed251d76750318961
Felix Walter <felix.walter@d3tn.com>
2023-02-17 09:45:17 +0100

    CI: test/ion_interoperability: Add tests for the ipn: scheme
    This makes the ion interop testing script flexible such that we can
    specify the EID scheme to be used. Two additional CI tests are added for
    the ipn scheme. Note that these use the newest version of ION while
    there still seems to be incompatibility for dtn EIDs with these (see
    issue #116).
    
    Signed-off-by: Felix Walter <felix.walter@d3tn.com>

--- test/ion_interoperability/minimal_forwarding_test/run.sh
+++ test/ion_interoperability/minimal_forwarding_test/run.sh
@@ -11,6 +11,31 @@ set -euo pipefail
 
 BP_VERSION=$1
 
+case "$2" in
+    dtn)
+        ION_SCRIPT_NAME=dtn.rc
+        ION_EID=ipn:1.0
+        UD3TN1_EID=dtn://ud3tn1.dtn/
+        UD3TN2_EID=dtn://ud3tn2.dtn/
+        SOURCE_AGENTID=source
+        SINK_AGENTID=sink
+        SINK_EID=dtn://ud3tn2.dtn/sink
+        ;;
+    ipn)
+        ION_SCRIPT_NAME=ipn.rc
+        ION_EID=ipn:1.0
+        UD3TN1_EID=ipn:2.0
+        UD3TN2_EID=ipn:3.0
+        SOURCE_AGENTID=1
+        SINK_AGENTID=1
+        SINK_EID=ipn:3.1
+        ;;
+    *)
+        echo "Usage: $0 (6|7) (dtn|ipn)"
+        exit 1
+        ;;
+esac
+
 # This assumes you are running the command from within the "ud3tn" directory.
 UD3TN_DIR="$(pwd)"
 cd "$UD3TN_DIR"
@@ -44,26 +69,26 @@ rm -f ion.log
 rm -f /tmp/ion*log /tmp/ud3tn*.log
 
 # Start first uD3TN instance (uD3TN1)
-"$UD3TN_DIR/build/posix/ud3tn" -s $UD3TN_DIR/ud3tn1.socket -c "tcpclv3:127.0.0.1,4555" -b $BP_VERSION -e "dtn://ud3tn1.dtn/" > /tmp/ud3tn1.log 2>&1 &
+"$UD3TN_DIR/build/posix/ud3tn" -s $UD3TN_DIR/ud3tn1.socket -c "tcpclv3:127.0.0.1,4555" -b $BP_VERSION -e "$UD3TN1_EID" > /tmp/ud3tn1.log 2>&1 &
 UD3TN1_PID=$!
 
 # Start second uD3TN instance (uD3TN2)
-"$UD3TN_DIR/build/posix/ud3tn" -s $UD3TN_DIR/ud3tn2.socket -c "tcpclv3:127.0.0.1,4554" -b $BP_VERSION -e "dtn://ud3tn2.dtn/" > /tmp/ud3tn2.log 2>&1 &
+"$UD3TN_DIR/build/posix/ud3tn" -s $UD3TN_DIR/ud3tn2.socket -c "tcpclv3:127.0.0.1,4554" -b $BP_VERSION -e "$UD3TN2_EID" > /tmp/ud3tn2.log 2>&1 &
 UD3TN2_PID=$!
 
 # Start ION instance
 ulimit -n 512 # fix behavior on systems with a huge limit (e.g. if the container runtime does not change the kernel default), see: #121
-ionstart -I test/ion_interoperability/minimal_forwarding_test/ionstart.rc
+ionstart -I test/ion_interoperability/minimal_forwarding_test/$ION_SCRIPT_NAME
 
 # UD3TN1_PID and UD3TN2_PID must be defined for this
 trap exit_handler EXIT
 
 # Configure a contact to ION in uD3TN1 which allows to reach uD3TN2
 sleep 0.5
-python "$UD3TN_DIR/tools/aap/aap_config.py" --socket $UD3TN_DIR/ud3tn1.socket --schedule 1 3600 10000 --reaches "dtn://ud3tn2.dtn/" ipn:1.0 tcpclv3:127.0.0.1:4556
+python "$UD3TN_DIR/tools/aap/aap_config.py" --socket $UD3TN_DIR/ud3tn1.socket --schedule 1 3600 10000 --reaches "$UD3TN2_EID" "$ION_EID" tcpclv3:127.0.0.1:4556
 
 # Send a bundle to uD3TN1, addressed to uD3TN2
 PAYLOAD="THISISTHEBUNDLEPAYLOAD"
-python "$UD3TN_DIR/tools/aap/aap_send.py" --socket $UD3TN_DIR/ud3tn1.socket --agentid source "dtn://ud3tn2.dtn/sink" "$PAYLOAD" &
+python "$UD3TN_DIR/tools/aap/aap_send.py" --socket $UD3TN_DIR/ud3tn1.socket --agentid "$SOURCE_AGENTID" "$SINK_EID" "$PAYLOAD" &
 
-timeout 10 stdbuf -oL python "$UD3TN_DIR/tools/aap/aap_receive.py" --socket $UD3TN_DIR/ud3tn2.socket --agentid sink --count 1 --verify-pl "$PAYLOAD" --newline -vv
+timeout 10 stdbuf -oL python "$UD3TN_DIR/tools/aap/aap_receive.py" --socket $UD3TN_DIR/ud3tn2.socket --agentid "$SINK_AGENTID" --count 1 --verify-pl "$PAYLOAD" --newline -vv
