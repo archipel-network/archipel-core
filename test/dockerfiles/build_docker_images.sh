0389435a6c6702bf11c1e8a7b2803c73c3eb2f23
Felix Walter <felix.walter@d3tn.com>
2023-02-20 12:12:02 +0100

    CI: Add a Dockerfile for the other CI tests
    This adds a Dockerfile containing everything needed in our other tests
    (Clang, Python, Python dependencies, Netcat).
    
    See also: #16
    
    Signed-off-by: Felix Walter <felix.walter@d3tn.com>

--- test/dockerfiles/build_docker_images.sh
+++ test/dockerfiles/build_docker_images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # SPDX-License-Identifier: BSD-3-Clause OR Apache-2.0
 
-# Script to build the Docker images necessary for running the ION
-# interoperability tests. Run this from the uD3TN base directory!
+# Script to build the Docker images necessary for running uD3TN's CI
+# tests. Run this from the uD3TN base directory!
 
 set -euo pipefail
 
@@ -14,6 +14,7 @@ fi
 docker build -f test/dockerfiles/ion-interop --build-arg ION_FILE=ion-open-source-3.7.4.tar.gz -t ud3tn-ion-interop:3.7.4 .
 docker build -f test/dockerfiles/ion-interop --build-arg ION_FILE=ion-open-source-4.0.1.tar.gz -t ud3tn-ion-interop:4.0.1 .
 #docker build -f test/dockerfiles/ion-interop -t ud3tn-ion-interop:latest .
+docker build -f test/dockerfiles/ci-python-clang -t ci-python-clang:3.10-bullseye .
 
 echo "You may now push the images to the registry:"
 echo "\$ docker tag ud3tn-ion-interop:<tag> registry.gitlab.com/d3tn/ud3tn-docker-images/ion-interop:<tag>"
