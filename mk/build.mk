archipel-network/archipel-core

commit 602c4ce00ad9718a4001c9c6310fd80c83cfb1bd

mk: Only link dynamic library with --whole-archive
For the executable we should not use this option as it unnecessarily
increases binary size.

There might also be incompatibilities with Mac, see #126

Signed-off-by: Felix Walter <felix.walter@d3tn.com>

mk/build.mk
@@ -41,7 +41,8 @@ $(eval $(call addComponentWithRules,external/util/src))
 $(eval $(call generateComponentRules,components/daemon))
 $(eval $(call generateComponentRules,test/unit))
 
-build/$(PLATFORM)/libud3tn.so: LDFLAGS += $(LDFLAGS_LIB)
+build/$(PLATFORM)/libud3tn.so: LDFLAGS += $(LDFLAGS_LIB) -Wl,--no-whole-archive
+build/$(PLATFORM)/libud3tn.so: LDFLAGS_PRE += -Wl,--whole-archive
 build/$(PLATFORM)/libud3tn.so: LIBS = $(LIBS_libud3tn.so)
 build/$(PLATFORM)/libud3tn.so: $(LIBS_libud3tn.so) | build/$(PLATFORM)
 	$(call cmd,link)

mk/toolchain.mk
@@ -55,9 +55,10 @@ cmd_ar             = "$(AR)" rcs "$@" $(OBJECTS)
 
 quiet_cmd_link     = LINK    $@
 cmd_link           = "$(LD)" -o "$@" \
-                     -Wl,--whole-archive -Wl,--start-group \
+                     $(LDFLAGS_PRE) \
+                     -Wl,--start-group \
                      $(OBJECTS) $(LIBS) \
-                     -Wl,--end-group -Wl,--no-whole-archive \
+                     -Wl,--end-group \
                      $(LDFLAGS)
 
 quiet_cmd_mkdir    = MKDIR   $@

commit 2889a17f231c5d2873b7537cb2ba98da2d66faee

mk: Simplify passing linker flags and pass them in correct order
On some systems (incl. our x86_64 Debian based CI), the change of the
linker argument order in 602c4ce0 broke the build, as the flag was then
passed after the library options (i.e. `-lc -lm -lpthread`).

This change removes the modifications to the `LDFLAGS` and `LDFLAGS_PRE`
variables in `build.mk` by making use of the `LDFLAGS_EXECUTABLE` and
`LDFLAGS_LIB` variables directly in dedicated linker commands.

An additional variable `LDFLAGS_PRE_LIB` is introduced for adding
preceding flags specifically for the library build (as `LDFLAGS_PRE` is
not touched anymore in `build.mk`).

Signed-off-by: Felix Walter <felix.walter@d3tn.com>

mk/build.mk
@@ -34,16 +34,9 @@ $(eval $(call addComponentWithRules,external/util/src))
 $(eval $(call generateComponentRules,components/daemon))
 $(eval $(call generateComponentRules,test/unit))
 
-ifeq ($(EXPECT_MACOS_LINKER),1)
-build/$(PLATFORM)/libud3tn.so: LDFLAGS += $(LDFLAGS_LIB)
-build/$(PLATFORM)/libud3tn.so: LDFLAGS_PRE += -Wl,-all_load
-else
-build/$(PLATFORM)/libud3tn.so: LDFLAGS += $(LDFLAGS_LIB) -Wl,--no-whole-archive
-build/$(PLATFORM)/libud3tn.so: LDFLAGS_PRE += -Wl,--whole-archive
-endif
 build/$(PLATFORM)/libud3tn.so: LIBS = $(LIBS_libud3tn.so)
 build/$(PLATFORM)/libud3tn.so: $(LIBS_libud3tn.so) | build/$(PLATFORM)
-	$(call cmd,link)
+	$(call cmd,linklib)
 
 # STATIC LIB
 
@@ -55,7 +48,6 @@ build/$(PLATFORM)/libud3tn.a: $(LIBS_libud3tn.so) | build/$(PLATFORM)
 
 $(eval $(call addComponent,ud3tn,components/daemon))
 
-build/$(PLATFORM)/ud3tn: LDFLAGS += $(LDFLAGS_EXECUTABLE)
 build/$(PLATFORM)/ud3tn: LIBS = $(LIBS_ud3tn)
 build/$(PLATFORM)/ud3tn: $(LIBS_ud3tn) | build/$(PLATFORM)
 	$(call cmd,link)

mk/toolchain.mk
@@ -38,17 +38,21 @@ CXXFLAGS += -fno-exceptions -fno-rtti
 
 ASFLAGS += $(ARCH_FLAGS)
 
+ifeq ($(EXPECT_MACOS_LINKER),1)
+  LDFLAGS_PRE_LIB += -Wl,-all_load
+else
+  LDFLAGS_PRE += -Wl,--start-group
+  LDFLAGS += -Wl,--end-group
+  LDFLAGS_PRE_LIB += -Wl,--whole-archive
+  LDFLAGS_LIB += -Wl,--no-whole-archive
+endif
+
 LDFLAGS += $(ARCH_FLAGS) -lc -lm
 
 ifeq ($(TOOLCHAIN),clang)
   ifneq ($(CLANG_SYSROOT),)
     CPPFLAGS += --sysroot $(CLANG_SYSROOT)
   endif
-endif
-
-ifneq ($(EXPECT_MACOS_LINKER),1)
-  LDFLAGS_PRE += -Wl,--start-group
-  LDFLAGS += -Wl,--end-group
 endif
 
 # COMMANDS
@@ -75,7 +79,13 @@ quiet_cmd_link     = LINK    $@
 cmd_link           = "$(LD)" -o "$@" \
                      $(LDFLAGS_PRE) \
                      $(OBJECTS) $(LIBS) \
-                     $(LDFLAGS)
+                     $(LDFLAGS_EXECUTABLE) $(LDFLAGS)
+
+quiet_cmd_linklib  = LINK    $@
+cmd_linklib        = "$(LD)" -o "$@" \
+                     $(LDFLAGS_PRE) $(LDFLAGS_PRE_LIB) \
+                     $(OBJECTS) $(LIBS) \
+                     $(LDFLAGS_LIB) $(LDFLAGS)
 
 quiet_cmd_mkdir    = MKDIR   $@
 cmd_mkdir          = mkdir -p "$@"