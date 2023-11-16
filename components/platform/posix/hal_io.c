f02fdc042de5794dac84445f0cc07555f69b7a1f
Felix Walter <felix.walter@d3tn.com>
2023-11-16 13:53:44 +0100

    hal_io: Protect logging via mutex
    This resolves the race condition documented in #139.
    The log macros now each call only a single function which locks
    a binary semaphore during execution.
    
    Fixes: #139
    
    Signed-off-by: Felix Walter <felix.walter@d3tn.com>

--- components/platform/posix/hal_io.c
+++ components/platform/posix/hal_io.c
@@ -7,6 +7,9 @@
  *
  */
 
+#include "platform/hal_time.h"
+#include "platform/hal_semaphore.h"
+
 #include "ud3tn/result.h"
 
 #include <errno.h>
@@ -16,8 +19,12 @@
 #include <stdarg.h>
 #include <stdint.h>
 
+static Semaphore_t log_io_semph;
+
 enum ud3tn_result hal_io_init(void)
 {
+	log_io_semph = hal_semaphore_init_binary();
+	hal_semaphore_release(log_io_semph);
 	return UD3TN_OK;
 }
 
@@ -32,8 +39,47 @@ int hal_io_message_printf(const char *format, ...)
 	return rc;
 }
 
-void hal_io_print_error(const char *message, int error)
+static inline const char *get_log_level_name(const uint8_t level)
 {
+	switch (level) {
+	case 1:
+		return "ERROR";
+	case 2:
+		return "WARNING";
+	case 3:
+		return "INFO";
+	default:
+		return "DEBUG";
+	}
+}
+
+int hal_io_log_printf(const int level, const char *const file, const int line,
+		      const char *const format, ...)
+{
+	int rc;
+	va_list v;
+
+	hal_semaphore_take_blocking(log_io_semph);
+	hal_time_print_log_time_string();
+	fprintf(stderr, "[%s] ", get_log_level_name(level));
+	va_start(v, format);
+	rc = vfprintf(stderr, format, v);
+	va_end(v);
+	fprintf(stderr, " [%s:%d]\n", file, line);
+	fflush(stderr);
+	hal_semaphore_release(log_io_semph);
+	return rc;
+}
+
+void hal_io_log_perror(const char *component, const char *file, int line,
+		       const char *message, int error)
+{
+	hal_semaphore_take_blocking(log_io_semph);
+	hal_time_print_log_time_string();
+	fprintf(stderr, "[SYSTEM ERROR] in %s: ", component);
 	errno = error;
 	perror(message);
+	fprintf(stderr, " [%s:%d]\n", file, line);
+	fflush(stderr);
+	hal_semaphore_release(log_io_semph);
 }
--- include/platform/hal_io.h
+++ include/platform/hal_io.h
@@ -10,8 +10,6 @@
 #ifndef HAL_IO_H_INCLUDED
 #define HAL_IO_H_INCLUDED
 
-#include "platform/hal_time.h"
-
 #include "ud3tn/result.h"
 
 #include <stddef.h>
@@ -20,34 +18,19 @@
 
 extern uint8_t LOG_LEVEL;
 
-static inline const char *get_log_level_name(const uint8_t level)
-{
-	switch (level) {
-	case 1:
-		return "ERROR";
-	case 2:
-		return "WARNING";
-	case 3:
-		return "INFO";
-	default:
-		return "DEBUG";
-	}
-}
-
 #define LOGF_GENERIC(level, f_, ...) \
-({ \
-	__typeof__(level) _level = (level); \
-	if (_level <= LOG_LEVEL) { \
-		hal_time_print_log_time_string(); \
-		hal_io_message_printf( \
-			"[%s] " f_ " [%s:%d]\n", \
-			get_log_level_name(_level), \
-			__VA_ARGS__, \
-			__FILE__, (int)(__LINE__) \
+do { \
+	__typeof__(level) level_ = (level); \
+	if (level_ <= LOG_LEVEL) { \
+		hal_io_log_printf( \
+			level_, \
+			__FILE__, \
+			(int)(__LINE__), \
+			f_, \
+			__VA_ARGS__ \
 		); \
-		fflush(stderr); \
 	} \
-})
+} while (0)
 
 #define LOGF(...) LOGF_GENERIC(3, __VA_ARGS__)
 #define LOGF_ERROR(...) LOGF_GENERIC(1, __VA_ARGS__)
@@ -69,15 +52,15 @@ static inline const char *get_log_level_name(const uint8_t level)
 #define LOGA(message, actionid, itemid) \
 	LOGF("%s (a = %d, i = %d)", message, actionid, itemid)
 
-#define LOGERROR(component_, msg_, errno_) ({ \
-	hal_time_print_log_time_string(); \
-	hal_io_message_printf( \
-		"%s: System error [%s:%d] -> ", \
-		component_, __FILE__, (int)(__LINE__) \
-	); \
-	hal_io_print_error(msg_, errno_); \
-	fflush(stderr); \
-})
+#define LOG_ERRNO(component_, msg_, errno_) \
+	hal_io_log_perror( \
+		component_, \
+		__FILE__, \
+		(int)(__LINE__), \
+		msg_, \
+		errno_ \
+	)
+#define LOGERROR LOG_ERRNO
 
 /**
  * @brief hal_io_init Initialization of underlying OS/HW for I/O
@@ -94,10 +77,24 @@ enum ud3tn_result hal_io_init(void);
 int hal_io_message_printf(const char *format, ...);
 
 /**
- * @brief hal_io_perror Print a system error (i.e., saved errno) using perror()
- * @param message The message passed to perror()
- * @param error The error number obtained from errno
+ * @brief hal_io_log_printf Write a log message to the debug interface.
+ * @param level The log level - a value between 1 (ERROR) and 4 (DEBUG).
+ * @param file The file that triggered the log message.
+ * @param line The line that triggered the log message.
+ * @param format Same as in standard libc-printf().
  */
-void hal_io_print_error(const char *message, int error);
+int hal_io_log_printf(int level, const char *file, int line,
+		      const char *format, ...);
+
+/**
+ * @brief hal_io_log_perror Log a system error (i.e., saved errno).
+ * @param component The component in which the error occurred, e.g., "Router".
+ * @param file The file in which the error occurred.
+ * @param line The line in which the error occurred.
+ * @param message The message passed to perror().
+ * @param error The error number obtained from errno.
+ */
+void hal_io_log_perror(const char *component, const char *file, int line,
+		       const char *message, int error);
 
 #endif /* HAL_IO_H_INCLUDED */
