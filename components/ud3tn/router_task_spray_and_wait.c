04448b3076f45b800d0f3ca72c84e151566e85d9
EpicKiwi <me@epickiwi.fr>
2025-12-19 23:49:49 +0100

    Send everything to everyone

--- components/ud3tn/router_task_spray_and_wait.c
+++ components/ud3tn/router_task_spray_and_wait.c
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: BSD-3-Clause OR Apache-2.0
 
 #include "ud3tn/bundle.h"
-#include "ud3tn/bundle_fragmenter.h"
-#include "ud3tn/common.h"
 #include "ud3tn/node.h"
+#include "ud3tn/result.h"
 #include "ud3tn/router.h"
 #include "ud3tn/routing_table.h"
 
@@ -19,8 +18,34 @@
 
 enum router_result_status router_route_bundle(struct bundle *b)
 {
-	LOG_ERROR("router_route_bundle not yet implemented");
-	return ROUTER_RESULT_OK;
+	const uint64_t timestamp_ms = hal_time_get_timestamp_ms();
+	if (bundle_get_expiration_time_ms(b) < timestamp_ms) {
+		return ROUTER_RESULT_EXPIRED;
+	}
+
+	struct node_list* node_list = routing_table_get_node_list();
+
+	enum router_result_status status = ROUTER_RESULT_NO_ROUTE;
+
+	while (node_list != NULL) {
+		struct node* node = node_list->node;
+
+		struct contact_list* contact_list = node->contacts;
+		while(contact_list != NULL){
+			if(contact_list->data->active){
+				if(router_add_bundle_to_contact(contact_list->data, b) == UD3TN_FAIL) {
+					LOGF_ERROR("Failed to emit bundle %p to %s", b, node->eid);
+				} else {
+					status = ROUTER_RESULT_OK;
+				}
+				break;
+			}
+			contact_list = contact_list->next;
+		}
+		node_list = node_list->next;
+	}
+
+	return status;
 }
 
 #endif
